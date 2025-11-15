0c0e9596553c36d8ce7b062ba0f1b9d4e0dc6dc2
nmitsali <nmitsali@gmail.com>
2025-11-15 14:18:07 +0100

    Create Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+export const Header: React.FC = () => (
+  <header className="bg-white shadow-md">
+    <div className="container mx-auto px-4 py-4">
+      <h1 className="text-3xl font-bold text-teal-600 tracking-tight">
+        Yarn Genius 🧶
+      </h1>
+    </div>
+  </header>
+);
